a5b9dcf4b73ac2365ea93397db3c62cc6f589167
svozza <svozza@gmail.com>
2026-02-11 21:49:29 +0000

    Initial commit

---
+++ test/sample-powertools-ev-ent-handler-validation.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SamplePowertoolsEvEntHandlerValidation from '../lib/sample-powertools-ev-ent-handler-validation-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sample-powertools-ev-ent-handler-validation-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SamplePowertoolsEvEntHandlerValidation.SamplePowertoolsEvEntHandlerValidationStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
